1cc2a2bd94ee98ec6fc3588adfcfb1b0d65befcd
David Cristóbal Valero Croma <david.valero@usach.cl>
2023-03-23 12:51:41 -0300

    tabla de frecuencia ep2

--- EP02/EP02-respuesta-grupo4.R
+++ EP02/EP02-respuesta-grupo4.R
@@ -5,7 +5,7 @@
 
 
 #Definir ruta/directorio de trabajo
-setwd("C:/Users/Jaime Carrasco/Documents/Universidad/IME/EPS/IME-2023/EP02")
+setwd("C:/Users/david/OneDrive/Escritorio/universidad/10mo Semestre/IME/actividades semanales/IME-2023/EP02")
 
 
 #Importar dplyr y ggpubr para utlizar funcionalidades del paquete.
@@ -14,7 +14,7 @@ library(ggpubr)
 
 #Leer datos del archivo csv, se importará con read.csv2() porque el archivo está en español.
 
-datos <- read.csv2("EP02 Datos.csv")
+datos <- read.csv2("EP02 Datos.csv",stringsAsFactors = TRUE)
 
 # Filtrar para trabajar con los datos solicitados, que sea hombre y sean de zona urbana o rural
 datosHombre <- filter(datos, sexo == "Hombre")
@@ -25,6 +25,11 @@ datosRural <- filter(datos,zona == "Rural")
 
 contadorUrbano <- datosUrbano %>% count(ch1)
 contadorRural <- datosRural %>% count(ch1)
+
+tabla <-table(datosHombre[["zona"]],datosHombre[["ch1"]])
+
+#tabla <-table(datosHombre[["ch1"]],datosHombre[["zona"]])
+tablaFrecuencia <- data.frame(tabla) %>% group_by(Var2)
 
 # g1 <- gghistogram(contadorUrbano,
 #                   x = "n",
@@ -40,25 +45,32 @@ contadorRural <- datosRural %>% count(ch1)
 #                 ylabel = "Cantidad")
 # le pedí ayuda al chat gpt y me dijo lo mismo q ya tenía hecho, pero no encuentro el error KJDNKJBDF
 
-g1 <- ggbarplot(contadorUrbano,
-                x = "ch1",
-                y = "n",
-                title = "Ocupación en zona urbana (Hombres)",
-                xlabel = "Ocupación",
-                ylabel = "Cantidad")
+# mis_colores <- c("blue", "red", "green", "purple")
+# g1 <- ggbarplot(contadorUrbano,
+#                 x = "ch1",
+#                 y = "n",
+#                 fill = mis_colores,
+#                 title = "Ocupación en zona urbana (Hombres)",
+#                 xlabel = "Ocupación",
+#                 ylabel = "Cantidad")
 
 
-g2 <- ggbarplot(contadorRural,
-                x = "ch1",
-                y = "n",
-                title = "Ocupación en zona rural (Hombres)",
-                xlabel = "Ocupación",
-                ylabel = "Cantidad")
+# g2 <- ggbarplot(contadorRural,
+#                 x = "ch1",
+#                 y = "n",
+#                 title = "Ocupación en zona rural (Hombres)",
+#                 xlabel = "Ocupación",
+#                 ylabel = "Cantidad")
+# 
+# 
+# print(g1)
+# print(g2)
 
+g3 <- ggplot(tablaFrecuencia, aes(fill=Var1,y=Freq,x=Var2))
+# g3 <- g3 + labs(y="proporción") + ggtitle("aaaaa")
+# g3 <- g3 + theme_pubr()
 
-print(g1)
-print(g2)
-
+print(g3)
 # ¿Se distribuye de igual manera la situación ocupacional de los hombres que viven en áreas rurales y quienes
 # viven en áreas urbanas de la RM?
 
