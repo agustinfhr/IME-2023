16d7acd1b4cc43ac340b474980a9539bbe722730
JimCarrasc0 <jaime.carrasco.q@usach.cl>
2023-06-05 18:38:06 -0400

    corregida ortografia ep08

--- EP08/EP08-respuesta-grupo-3.R
+++ EP08/EP08-respuesta-grupo-3.R
@@ -75,7 +75,7 @@ library(car)
 
 # ------------------------------- DATOS ----------------------------------------
 
-dir <- "C:/Users/agust/Desktop/REPO IME/IME-2023/EP08"
+dir <- "C:/Users/Jaime Carrasco/Documents/Universidad/IME/EPS/IME-2023/EP08"
 basename <- "EP08 Datos.csv"
 file <- file.path(dir, basename)
 poblacion <- read.csv2(file = file)
@@ -86,46 +86,39 @@ poblacion <- read.csv2(file = file)
 # En este momento, los agr√≥nomos buscan determinar si existen diferencias en el peso a la semana 20 de
 # crecimiento entre las manzanas Pink Lady, Starking, Golden y Granny Smith.
 
-# 1. El equipo obtiene y manipula correctamente los datos entregados y propone hipÛtesis pertinentes a
+# 1. El equipo obtiene y manipula correctamente los datos entregados y propone hip?tesis pertinentes a
 # contrastar para cada problema.
 
-# HipÛtesis nula H0: No hay diferencias significativas en los pesos de las manzanas Pink Lady,
+# Hip√≥tesis nula H0: No hay diferencias significativas en los pesos de las manzanas Pink Lady,
 # Starking, Golden y Granny Smith en la semana 20 de crecimiento.
 # 
-# HipÛtesis alternativa HA: Hay al menos una diferencia significativa en los pesos de las manzanas
+# Hip√≥tesis alternativa HA: Hay al menos una diferencia significativa en los pesos de las manzanas
 # Pink Lady, Starking, Golden y Granny Smith en la semana 20 de crecimiento.
 # 
-# FormulaciÛn Matem·tica
+# Formulaci√≥n Matem√°tica
 # 
-# H0: ????? = ????? = ????? = ?????
+# H0: mu1 = mu2 = mu3 = mu4
 # 
-# HA: Al menos una desigualdad en el conjunto {????? ??? ?????, ????? ??? ?????, ????? ??? ?????, ????? ??? ?????, ????? ??? ?????, ????? ??? ?????}
+# HA: Al menos una desigualdad en el conjunto {mu1 != mu2, mu1 != mu3, mu1 != mu4, mu2 != mu3, mu2 != mu4, mu3 != mu4}
 
 semana20 <- subset(poblacion, tiempo == "semana_20" & variedad %in% c("Pink Lady",
                                                                       "Starking", 
                                                                       "Golden", 
                                                                       "Granny Smith"))
 
-# Lo comento porque no me funcionaba :(
-# grupo20 <- poblacion %>% filter (tiempo == "semana_20")
-# mz_PL <- grupo20 %>% filter (variedad == "Pink Lady")
-# mz_Starking <- grupo20 %>% filter (variedad == "Starking")
-# mz_Golden <- grupo20 %>% filter (variedad == "Golden")
-# mz_GS <- grupo20 %>% filter (variedad == "Granny Smith")
 
+# 2. El equipo se asegura que cada caso cumpla las condiciones para utilizar ANOVA con validez, usando gr√°ficos
+# o pruebas estad√≠sticas auxiliares disponibles en el entorno R.
 
-# 2. El equipo se asegura que cada caso cumple las condiciones para utilizar ANOVA con validez, usando gr·ficos
-# o pruebas estadÌsticas auxiliares disponibles en el entorno R.
-
-# Comprobar la 4 Condiciones para ANOVA.
+# Comprobar las 4 Condiciones para ANOVA.
 
 # Primero, el peso de las manzanas (variable dependiente) se mide en gramos, 
-# que es una escala de razÛn (porque tiene un cero absoluto y los intervalos son iguales).
+# que es una escala de raz√≥n (porque tiene un cero absoluto y los intervalos son iguales).
 # por lo que si se cumple la primera condicion.
 
-# Segundo, se asume que las observaciones son independientes entre si, cumpliendo la condicion de Independencia
+# Segundo, se asume que las observaciones son independientes entre si, cumpliendo la condici√≥n de Independencia
 
-# Tercero, comprobar que los datos siguen una distribucion normal
+# Tercero, comprobar que los datos siguen una distribuci√≥n normal
 
 # Prueba de Shapiro-Wilk para la normalidad
 shapiro.test(semana20$peso[semana20$variedad == "Pink Lady"])
@@ -134,10 +127,10 @@ shapiro.test(semana20$peso[semana20$variedad == "Golden"])
 shapiro.test(semana20$peso[semana20$variedad == "Granny Smith"])
 
 # Como se puede observar, todos los p-valores son mayores al nivel de significancia (p<0.05)
-# No se rechaza la hipotesis nula de la prueba de Shapiro, por lo que el principio de normalidad
+# No se rechaza la hip√≥tesis nula de la prueba de Shapiro, por lo que el principio de normalidad
 # se cumple para estas cuatro variedades.
 
-# Grafico Q-Q, para visualizar la distribucion normal. 
+# Grafico Q-Q, para visualizar la distribuci√≥n normal. 
 qqnorm(semana20$peso[semana20$variedad == "Pink Lady"])
 qqline(semana20$peso[semana20$variedad == "Pink Lady"])
 
@@ -150,20 +143,20 @@ qqline(semana20$peso[semana20$variedad == "Golden"])
 qqnorm(semana20$peso[semana20$variedad == "Granny Smith"])
 qqline(semana20$peso[semana20$variedad == "Granny Smith"])
 
-# A partir del grafico Q-Q, tambien se puede obversar que los datos tienden a distribuirse de forma normal.
+# A partir del grafico Q-Q, tambi√©n se puede obversar que los datos tienden a distribuirse de forma normal.
 
-# Cuarto, verificar la condicion de homocedasticidad(tienen la misma varianza) en los grupos para poder aplicar el ANOVA.
+# Cuarto, verificar la condici√≥n de homocedasticidad (tienen la misma varianza) en los grupos para poder aplicar el ANOVA.
 
-# Para comprobar esta condicion, se puede utilizar la prueba de Levene
+# Para comprobar esta condici√≥n, se puede utilizar la prueba de Levene
 leveneTest(peso ~ variedad, data = semana20)
 
 # En este caso, el valor p obtenido por la prueba de Levene es de 0.7712, mayor al nivel de 
-# significancia 0.05, por lo que indica que no se puede rechazar la hipotesis nula de que las 
+# significancia 0.05, por lo que indica que no se puede rechazar la hip√≥tesis nula de que las 
 # varianzas son iguales en los grupos, de esta manera, cumpliendo con la condicion de homocedasticidad.
 
 # Finalmente, los datos cumplen las cuatro condiciones para realizar una prueba ANOVA.
 
-# 3. El equipo realiza de forma correcta y completa una prueba ANOVA Ûmnibus para cada problema.
+# 3. El equipo realiza de forma correcta y completa una prueba ANOVA √ìmnibus para cada problema.
 
 # Ahora procediendo a ejecutar la prueba ANOVA.
 
@@ -173,33 +166,33 @@ summary(modelo_anova)
 
 # A partir de los resultados obtenido por la prueba ANOVA
 # el p-valor es 0.699, mayor que 0.05. Esto significa que no se puede rechazar
-# la hipÛtesis nula de que no hay diferencias en los pesos de las manzanas Pink Lady,
+# la hip√≥tesis nula de que no hay diferencias en los pesos de las manzanas Pink Lady,
 # Starking, Golden y Granny Smith. 
 
 # Por lo tanto, no hay evidencia de que existan diferencias significativas 
 # en los pesos de estas cuatro variedades de manzanas en la semana 20 de crecimiento.
 
-# 4. El equipo determina correctamente si corresponde o no hacer un an·lisis post-hoc en cada caso, el que
+# 4. El equipo determina correctamente si corresponde o no hacer un an√°lisis post-hoc en cada caso, el que
 # aplica de forma correcta y completa (cuando corresponda).
 
 # En este caso, el p-valor de la prueba ANOVA fue 0.699, que es mayor que 0.05. 
-# Esto significa que no se rechaza la hipÛtesis nula de que no hay diferencias significativas 
-# entre las medias de los grupos. Por lo tanto, no serÌa necesario realizar un an·lisis post-hoc
+# Esto significa que no se rechaza la hip√≥tesis nula de que no hay diferencias significativas 
+# entre las medias de los grupos. Por lo tanto, no ser√° necesario realizar un an√°lisis post-hoc
 # porque no se ha encontrado diferencias significativas entre los grupos en el ANOVA.
 # 
-# A este punto, se est· cumpliendo con el objetivo de la actividad en tÈrminos 
-# de determinar si corresponde o no realizar un an·lisis post-hoc. Sin embargo, si el p-valor 
-# hubiera sido menor que 0.05, entonces se deberÌa proceder con un an·lisis post-hoc, como la prueba
-# de Tukey, para determinar dÛnde est·n exactamente esas diferencias entre los grupos.
+# A este punto, se est√° cumpliendo con el objetivo de la actividad en t√©rminos 
+# de determinar si corresponde o no realizar un an√°lisis post-hoc. Sin embargo, si el p-valor 
+# hubiera sido menor que 0.05, entonces se deber√≠a proceder con un an√°lisis post-hoc, como la prueba
+# de Tukey, para determinar d√≥nde est√°n exactamente esas diferencias entre los grupos.
 
 # 5. El equipo interpreta adecuadamente los resultados de las pruebas y responde las preguntas planteadas en
-# cada caso, bas·ndose en la prueba Ûmnibus y, si corresponde, considerando tambiÈn el an·lisis post-hoc.
+# cada caso, bas√°ndose en la prueba √ìmnibus y, si corresponde, considerando tambi√©n el an√°lisis post-hoc.
 
-# Bas·ndonos en nuestro an·lisis ANOVA, concluimos que no hay diferencias significativas en el peso
+# Bas√°ndonos en nuestro an√°lisis ANOVA, concluimos que no hay diferencias significativas en el peso
 # de las manzanas Pink Lady, Starking, Golden y Granny Smith en la semana 20 de crecimiento.
-# Esto se debe a que nuestro p-valor de 0.699 es mayor que el nivel de significaciÛn de 0.05,
-# lo que significa que no podemos rechazar la hipÛtesis nula de que las medias de los grupos son
-# iguales. Por lo tanto, no hay evidencia de que el peso de las manzanas varÌe entre estas cuatro 
+# Esto se debe a que nuestro p-valor de 0.699 es mayor que el nivel de significaci√≥n de 0.05,
+# lo que significa que no podemos rechazar la hip√≥tesis nula de que las medias de los grupos son
+# iguales. Por lo tanto, no hay evidencia de que el peso de las manzanas var√≠e entre estas cuatro 
 # variedades en la semana 20 de crecimiento.
 
 
